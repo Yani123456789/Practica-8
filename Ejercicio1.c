bb488a3ef98593e19d48f562220e35af379c25f0
Yani123456789 <yaniarrazola54@gmail.com>
2026-04-09 13:34:08 -0600

    Update Ejercicio1.c

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -10,3 +10,20 @@ int main()
     printf("%d x %d = %d\n", num, cont, num*cont); 
   return 0;
 }
+
+o
+
+#include <stdio.h>
+int main() 
+{
+  int num, cont = 0;
+  printf("\a----- Tabla de multiplicar -----\n"); 
+  printf("Ingrese un número: \n");
+  scanf("%d", &num);
+  printf("La tabla de multiplicar del %d es:\n", num); 
+  while (cont <= 10)
+  {
+    printf("%d x %d = %d\n", num, cont, num*cont); 
+    cont = cont + 1;
+  }
+}
